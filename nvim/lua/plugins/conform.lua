f158ad614e5111161aa4ecf2f96ea8a823b4b58a
idalen <danielmv829@gmail.com>
2025-05-27 13:33:53 -0300

    Include conform

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,13 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 1000,
+    },
+    formatters_by_ft = {
+      go = { "goimports", "gofmt" },
+      lua = { "stylua" },
+    },
+  },
+}
